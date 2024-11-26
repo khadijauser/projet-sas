e635ffc1f26ca4469bed0e7a9dfcdd3df429e007
khadijauser <khadijauser779@gmail.com>
2024-11-26 15:02:09 +0100

    complete code

--- c2.c
+++ c2.c
@@ -29,14 +29,13 @@ while (1)
     printf("Enter date DD/MM/YYYY : ");
     scanf("%10s",listTask[numberTask].dueDate);
 if (sscanf(listTask->dueDate,"%2d/%2d/%4d",&day,&month,&year) != 3 ||day<1|| day>31 || 
-month< 1 || month >12 || 
-year>2050 ||year < 2024)
+                                                               month< 1 || month >12 || 
+                                                              year>2050 ||year < 2024)
 {
      printf("Format invalid\n");
 }else break; 
  
 }
-
 while (1)
 {
 printf("Enter your priority (High/Low) : ");
@@ -46,7 +45,6 @@ if (strcmp(listTask->priority ,"high")==0|| strcmp(listTask->priority,"low")==0)
 break;
 }else printf("\nInvalid\n");
 
-    
 }
 
 };
@@ -54,65 +52,126 @@ break;
 void DisplayTask(struct task listTask){
   if (listTask.id!=0){
 
-  
+        printf("\n--------------------------------\n");
   printf("id : %d \n title :%s \n descreption  : %s\n Duedate : %s\n priority : %s\n",
                 listTask.id ,
                 listTask.title,
                 listTask.descruption,
                 listTask.dueDate,
                 listTask.priority);
-  
+        printf("\n--------------------------------\n");
   }
+}
+//fonction update
+void update(struct task *listTask,char nv[],int field){
   
+  switch (field)
+  {
+  case 1:
+    strcpy(listTask->descruption,nv);
+    break;
+  case 2:
+    strcpy(listTask->dueDate,nv);
+  break;
+  case 3:
+    strcpy(listTask->priority,nv);
+  break;
+  
+  default:
+  printf("invalide field\n");
+        printf("\n--------------------------------\n");
+
+    break;
+  }
+}
+
+//fonction delete
+void delete (struct task *listTask){
+    listTask->id=0;
+}
+
+//fonction filter 
+void filter(struct task listTask[],char fp[], int id){ //fp =filter priority
+int found = 0;
+  for (int i = 0; i <id; i++)
+  {
+    if (strcmp (listTask[i].priority,fp) == 0)
+    {
+      DisplayTask(listTask[i]);
+      found= 1;
+    }
+  }
+   if (!found)
+    {
+      printf(" Not available ");
+        printf("\n--------------------------------\n");
+
+    }
 }
 
 int main(){
-    int id=0;
-
+  int id=0,id_modify,id_delete;
+  char nv[100];
   struct task listTask[MAX];
   int numberTask=0;
-  int choice;
+  int choice,field;
+  char fp[4];
   do {
+        printf("\n--------------------------------\n");
+
         printf("----- MENU --------\n");
         printf("1: Add task to table\n");
         printf("2: Display the task table\n");
         printf("3: Modify a task\n");
         printf("4: Delete a task\n");
-        printf("5: Exit the program\n");
-        printf("Enter choice: ");
+        printf("5: Filtred\n");
+        printf("0: Exit the program\n");
+        printf("Enter choice: \n");
+        printf("\n--------------------------------\n");
+
         scanf("%d", &choice);
         switch (choice) {
+          case 0:
+          break;
             case 1: 
-          
-            
+        
                 addTask(&listTask[id],id+1);
               id++;
-
-            
             break;
             case 2: 
             for (int i = 0; i < id; i++)
             {
-                  DisplayTask(listTask[i]);
+                DisplayTask(listTask[i]);
 
             }
             
              break;
+case 3:
 
-            
+  printf("Enter the  id of variabel you want to change :");
+  scanf("%d",&id_modify);
 
+  printf("For update \n1.descruption \n2.duedate \n3.priority \n");
+  scanf("%d",&field);
+  printf("Enter new value : \n");
+  scanf("%s",&nv);
+  update(&listTask[id_modify-1],nv,field);
+  //printf("invalid field");
+break;
 
+case 4:
+     printf("Enter id task to delete :");
+     scanf("%d",&id_delete);
 
+     delete(&listTask[id_delete-1]);
 
+break;
 
-
-           // case 3: updateTask(); break;
-            case 4:
-                printf("Enter task ID to delete: ");
-                scanf("%d", &id);
-               // deleteTask(id - 1); // Adjust for zero-based index
-                break;
-            case 5: printf ("Have a good day");
+          
+            case 5: 
+            printf("Enter high or low ");
+            scanf("%s",fp);
+            filter(listTask,fp,id);
             break; // Exit
             default: printf("Invalid choice.\n");
         }
@@ -120,9 +179,3 @@ int main(){
     
     return 0;
 }
-
-  
-
-
-
-
